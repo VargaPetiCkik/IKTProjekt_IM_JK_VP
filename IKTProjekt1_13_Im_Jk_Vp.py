f0ea19371da4f7af5103aaafbcb578388363865d
juhasz.kristof <juhasz.kristof@ckik.loc>
2024-12-19 12:05:02 +0100

    harmadik

--- IKTProjekt1_13_Im_Jk_Vp.py
+++ IKTProjekt1_13_Im_Jk_Vp.py
@@ -13,3 +13,21 @@ print(f"{j:.0f}% lett jobb mint 3-as")
 
 
 #2 feladat
+ 
+n = 10
+t = [3, 37, 18, 38, 14, 23, 28, 27, 39, 40]  
+
+otos = 0
+egyes = 0
+
+
+for i in t:
+    if i >= 34:  
+        otos += 1
+    elif i < 12:  
+        egyes += 1
+if otos > egyes:
+    print("Igen, több ötös lett, mint egyes.")
+else:
+    print("Nem, nem több ötös lett, mint egyes.")
+    
