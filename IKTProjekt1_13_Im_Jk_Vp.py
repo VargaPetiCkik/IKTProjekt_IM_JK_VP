b57588faca9354fce8d721fc7961b2fb514b7b69
juhasz.kristof <juhasz.kristof@ckik.loc>
2024-12-19 11:52:50 +0100

    masodik

--- IKTProjekt1_13_Im_Jk_Vp.py
+++ IKTProjekt1_13_Im_Jk_Vp.py
@@ -1,4 +1,15 @@
+#1 feladat
 n={3, 14, 18, 32, 14, 23, 28, 27, 26, 40}
-for i in range(n):
-    if()
 
+j=0
+
+for i in n:
+    if(i>20):
+        j+=5
+print(f"{j:.0f}% lett jobb mint 3-as")
+    
+
+
+
+
+#2 feladat
